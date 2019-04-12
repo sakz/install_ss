5fc0e3ec63a7a8010e727091ba2220399102313c
jiazhao <fayecy@qq.com>
2019-04-12 14:34:05 +0800

    supervisor 3.3.5

--- supervisor.sh
+++ supervisor.sh
@@ -3,7 +3,7 @@
 baseUrl=https://raw.githubusercontent.com/sakz/install_ss/master/
 
 yum install -y python-setuptools
-easy_install -i https://pypi.tuna.tsinghua.edu.cn/simple supervisor
+easy_install -i https://pypi.tuna.tsinghua.edu.cn/simple "supervisor==3.3.5"
 wget -P /etc  ${baseUrl}supervisord.conf
 unlink /tmp/supervisor.sock
 supervisord -c /etc/supervisord.conf
