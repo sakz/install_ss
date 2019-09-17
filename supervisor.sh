20c6b6b6fb0e94b7292b0e22e633d9455fcd9749
jiazhao <fayecy@qq.com>
2019-09-17 10:46:49 +0800

    fix install supervisor

--- supervisor.sh
+++ supervisor.sh
@@ -3,6 +3,7 @@
 baseUrl=https://raw.githubusercontent.com/sakz/install_ss/master/
 
 yum install -y python-setuptools
+easy_install -i https://pypi.tuna.tsinghua.edu.cn/simple "meld3==1.0.2"
 easy_install -i https://pypi.tuna.tsinghua.edu.cn/simple "supervisor==3.3.5"
 wget -P /etc  ${baseUrl}supervisord.conf
 unlink /tmp/supervisor.sock
