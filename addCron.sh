7851b4394e6054c84b2d2a3286d4d74ff18ccb9f
jiazhao <fayecy@qq.com>
2020-07-06 15:35:34 +0800

    update

--- addCron.sh
+++ addCron.sh
@@ -5,5 +5,8 @@ baseUrl=https://raw.githubusercontent.com/sakz/install_ss/master/
 wget  ${baseUrl}checkV2ray.sh
 wget  ${baseUrl}cleanDockerLog.sh
 
+chmod +x checkV2ray.sh
+chmod +x cleanDockerLog.sh
+
 echo "* * * * * bash /root/checkV2ray.sh" >> /var/spool/cron/root
-echo "0 * * * * bash /root/cleanDockerLog.sh" >> /var/spool/cron/root
+echo "0 0 * * * bash /root/cleanDockerLog.sh" >> /var/spool/cron/root
