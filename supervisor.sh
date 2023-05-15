b6a9809e08b09f434f04282973e2be749b82e770
sakz <fayecy@qq.com>
2023-05-16 01:03:36 +0800

    update

--- supervisor.sh
+++ supervisor.sh
@@ -3,8 +3,8 @@
 baseUrl=https://raw.githubusercontent.com/sakz/install_ss/master/
 
 yum install -y python-setuptools
-easy_install -i https://pypi.tuna.tsinghua.edu.cn/simple "meld3==1.0.2"
-easy_install -i https://pypi.tuna.tsinghua.edu.cn/simple "supervisor==3.3.5"
+easy_install -i https://pypi.org/simple/ "meld3==1.0.2"
+easy_install -i https://pypi.org/simple/ "supervisor==3.3.5"
 wget -P /etc  ${baseUrl}supervisord.conf
 unlink /tmp/supervisor.sock
 supervisord -c /etc/supervisord.conf
