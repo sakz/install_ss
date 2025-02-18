53e4a82faaaab6192eee0698504e9dd28d1dfc4f
v <v@v.com>
2025-02-19 02:29:31 +0800

    1

--- addCron.sh
+++ addCron.sh
@@ -8,8 +8,19 @@ wget  ${baseUrl}cleanDockerLog.sh
 chmod +x checkV2ray.sh
 chmod +x cleanDockerLog.sh
 
-echo "* * * * * bash /root/checkV2ray.sh" >> /var/spool/cron/root
-echo "0 0 * * * bash /root/cleanDockerLog.sh" >> /var/spool/cron/root
-echo "33 0 * * * /root/.acme.sh/acme.sh --cron --home /root/.acme.sh > /dev/null" >> /var/spool/cron/root
-echo "35 4 * * * /etc/nginx/sbin/nginx -s reload" >> /var/spool/cron/root
-echo "0 */2 * * * rm -rf /etc/nginx/logs/*.log" >> /var/spool/cron/root
+
+(
+    crontab -l 2>/dev/null
+    echo "* * * * * bash /root/checkV2ray.sh"
+    echo "0 0 * * * bash /root/cleanDockerLog.sh"
+    echo "33 0 * * * /root/.acme.sh/acme.sh --cron --home /root/.acme.sh > /dev/null"
+    echo "35 4 * * * /etc/nginx/sbin/nginx -s reload"
+    echo "0 */2 * * * rm -rf /etc/nginx/logs/*.log"
+) | crontab -
+
+
+# echo "* * * * * bash /root/checkV2ray.sh" >> /var/spool/cron/root
+# echo "0 0 * * * bash /root/cleanDockerLog.sh" >> /var/spool/cron/root
+# echo "33 0 * * * /root/.acme.sh/acme.sh --cron --home /root/.acme.sh > /dev/null" >> /var/spool/cron/root
+# echo "35 4 * * * /etc/nginx/sbin/nginx -s reload" >> /var/spool/cron/root
+# echo "0 */2 * * * rm -rf /etc/nginx/logs/*.log" >> /var/spool/cron/root
